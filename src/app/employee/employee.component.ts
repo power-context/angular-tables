197ff7ce62c421a6bed33c089d098cf4e6bc9e32
unknown <lobbyc@LobbyC-PC.hhtowers.local>
2020-01-31 22:12:51 +0200

    Add form

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeServiceService } from '../shared/employee-service.service';
+import { Component, OnInit } from "@angular/core";
+import { EmployeeServiceService } from "../shared/employee-service.service";
 
 @Component({
-  selector: 'app-employee',
-  templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  selector: "app-employee",
+  templateUrl: "./employee.component.html",
+  styleUrls: ["./employee.component.css"]
 })
 export class EmployeeComponent implements OnInit {
+  departments: any[] = [
+    { id: 1, value: "Dep 1" },
+    { id: 2, value: "Dep 2" },
+    { id: 3, value: "Dep 3" }
+  ];
 
-  constructor(public service: EmployeeServiceService) { }
+  constructor(public service: EmployeeServiceService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  submit(){
-    
-  }
-
+  submit() {}
 }
--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import * as Materials from '@angular/material';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import * as Materials from "@angular/material";
+
+const materialImport = [
+  Materials.MatToolbarModule,
+  Materials.MatGridListModule,
+  Materials.MatInputModule,
+  Materials.MatFormFieldModule,
+  Materials.MatRadioModule,
+  Materials.MatSelectModule,
+  Materials.MatDatepickerModule,
+  Materials.MatNativeDateModule,
+  Materials.MatCheckboxModule,
+  Materials.MatButtonModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    Materials.MatToolbarModule,
-    Materials.MatGridListModule,
-    Materials.MatInputModule,
-    Materials.MatFormFieldModule,
-    Materials.MatRadioModule
-  ],
-  exports: [
-    Materials.MatToolbarModule,
-    Materials.MatGridListModule,
-    Materials.MatInputModule,
-    Materials.MatFormFieldModule,
-    Materials.MatRadioModule
-  ]
+  imports: [CommonModule, materialImport],
+  exports: [materialImport]
 })
-export class MaterialsModule { }
+export class MaterialsModule {}
